97b3b1db7d4aa144188e404719d84657dcc1743c
GitHub Action <action@github.com>
2022-09-24 12:46:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/huchunnuan/hcnideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/huchunnuan/hcnideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.huchunnuan.hcnideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.huchunnuan.hcnideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
